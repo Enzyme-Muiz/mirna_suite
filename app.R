14551cd3dabe018e9f093ce5406ebb6336e8a882
Muizdeen Raji <muizdeenraji@gmail.com>
2020-08-11 17:16:59 +0100

    sidebarlayout_use_and_panels

--- app.R
+++ app.R
@@ -6,39 +6,121 @@ ui<- fluidPage(
                   tabPanel(title= "miRNA location",
                 fluidRow
                (
-                column(6,align="center",tags$em(tags$h1("MicroRNA Suite"))),
-                 column(6, align= "center", tags$img(height=150, width=400, src= "miRNA.jpg"))
+                column(6,align="center", tags$div(class= "fade",
+                  tags$em(tags$h1("MicroRNA Suite")))),
+                 #column(6, align= "center", tags$img(height=150, width=400, src= "miRNA.jpg"))
                  ),
-              fluidRow
-              (column(6,
-               textInput(inputId="num2", label="mirna input", value = "hsa-let-7c-3p"), 
-               textOutput("selected_var")
+              sidebarLayout
+              (
+
+                sidebarPanel(
+               textInput(inputId="num2", label="mirna input", value = "hsa-let-7c-3p")),
+               mainPanel(align= "center",
+               textOutput("selected_var"))
               ),
-                column(6, sliderInput(inputId= "num", label= "choose a number", 
-                                      value =20, min= 1, max=100))
+                
                 
                   
                 
                 
-               ),
-               fluidRow(
-              column(6,
-              textInput('vec1', 'Enter a vector (comma delimited)', "hsa-miR-4665-5p"),
+               
+               sidebarLayout(
+              sidebarPanel(
+              textInput('vec1', 'mirna', "hsa-miR-4665-5p"),
+              sliderInput(inputId= "num", label= "choose a number", 
+                                      value =20, min= 1, max=100), 
+               
                actionButton(inputId = "go", label= "mirna_target")),
-              column(6, 
+              mainPanel(
                textOutput("oid1")
               )
                )
               ),
-              tabPanel(title= "cool",
-              selectInput("variable", label="select_input",choices=list('PathwayCommonsPathways'=1,'ReactomePathways'=2,'NetPath_Gene_regulation'=3,'KEGG_filtered_canonical_pathways'=4,'DisGeNet'=5,'gene_location'=6)),
+              tabPanel(title= "miRNA_set_analysis",
+              sidebarLayout(
+              sidebarPanel( textInput(inputId="num2", label="mirnas_input", value = c("hsa-let-7c-3p,hsa-miR-125b-5p")), 
+  
+              selectInput("variable", label="select_pathway",choices=list('PathwayCommonsPathways'=1,'ReactomePathways'=2,'NetPath_Gene_regulation'=3,'KEGG_filtered_canonical_pathways'=4,'DisGeNet'=5,'gene_location'=6)),
+              actionButton(inputId = "go2", label= "overrepresented_pathways")),
               
+              mainPanel(
+               textOutput("oid2")),
               
-               textOutput("oid2"),
-              
+                
+               )
+
                 )
                ),
-              shinyWidgets::setBackgroundColor("green"))    
+              shinyWidgets::setBackgroundColor("green"),
+
+              tags$head(
+              tags$style(HTML("
+                    #oid1{
+                      color: white;
+                      background: blue;
+                      font-family: 'Times New Roman', Times, serif;
+                      font-size: 10px;
+                      font-style: italic;
+                    }
+                    #selected_var2 {
+                      color: blue;
+                      background: orange;
+                      font-family: 'Times New Roman', Times, serif;
+                      font-size: 12px;
+                      font-weight: bold;
+                    }
+                    #rendertext1 {
+                      color: red;
+                      background: yellow;
+                      font-family: Arial, Helvetica, sans-serif;
+                      font-size: 19px;
+                    }
+                    
+                    .fade {opacity: 1;
+                      transition: opacity .25s ease-in-out;
+                      -moz-transition: opacity .25s ease-in-out;
+                      }
+
+                    .fade:hover{
+                      opacity:0;
+                    }
+
+
+                    .fade{
+                      
+                       transition: background-color;
+                      background-color: red;
+
+                    }
+
+                    .fade h1 {
+                      font-family: monospace; /* Web-safe typewriter-like font */
+                      overflow: hidden; /* Ensures the content is not revealed until the animation */
+                      border-right: .15em solid orange; /* The typwriter cursor */
+                      white-space: nowrap; /* Keeps the content on a single line */
+                      margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+                      letter-spacing: .15em; /* Adjust as needed */
+                      animation: 
+                        typing 3.5s steps(30, end),
+                        blinking-cursor .5s step-end infinite;
+                    }
+
+                    /* The typing effect */
+                    @keyframes typing {
+                      from { width: 0 }
+                      to { width: 100% }
+                    }
+
+                    /* The typewriter cursor effect */
+                    @keyframes blinking-cursor {
+                      from, to { border-color: transparent }
+                      50% { border-color: orange; }
+                    }
+
+
+                    
+                    ")))
+)
 
 server<- function(input, output)
   {
@@ -57,7 +139,7 @@ server<- function(input, output)
   (unique(miRNAtarget@data[,4])[c(1:input$num)])
   })
   
-  output$oid1<- renderPrint({
+  output$oid1<- renderText({
    print(data())
     
     }
